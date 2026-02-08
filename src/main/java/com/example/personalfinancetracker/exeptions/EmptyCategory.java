b03e11f1a2d5afc01785db0f27047d41fd1884d7
zhelez9aca <azeleznov95@gmail.com>
2026-02-08 12:52:42 +0300

    stable prototype

--- src/main/java/com/example/personalfinancetracker/exeptions/EmptyCategory.java
+++ src/main/java/com/example/personalfinancetracker/exeptions/EmptyCategory.java
@@ -2,6 +2,6 @@ package com.example.personalfinancetracker.exeptions;
 
 public class EmptyCategory extends RuntimeException{
     public EmptyCategory(String message){
-        super(message);
+        super(message);;
     }
 }
